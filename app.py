9cfb2ae18dcb081f1329d3b9d130c80c0474b9d5
Apolinário from multimodal AI art <multimodalart@users.noreply.huggingface.co>
2024-01-02 20:16:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import torch
 import spaces
-from diffusers import StableDiffusionPipeline, DDIMScheduler, AutoencoderKL
+from diffusers import StableDiffusionPipeline, DDIMScheduler, AutoencoderKL, AutoFeatureExtractor, StableDiffusionSafetyChecker
 from ip_adapter.ip_adapter_faceid import IPAdapterFaceID, IPAdapterFaceIDPlus
 from huggingface_hub import hf_hub_download
 from insightface.app import FaceAnalysis
